bfbc7d4e12cde228d475a14c5699e5da03adf107
Acorjuela <105765061+Acorjuela@users.noreply.github.com>
2025-03-30 00:59:01 -0500

    Update time.js

--- Js/time.js
+++ Js/time.js
@@ -3,7 +3,7 @@ const openModalButton = document.querySelector(".apceta");
 
 // Temporizador
 const countdown = () => {
-    const launchDate = new Date("2025-03-28T23:30:00Z").getTime();
+    const launchDate = new Date("2025-04-12T23:30:00Z").getTime();
     const now = new Date().getTime();
     const timeLeft = launchDate - now;
 
